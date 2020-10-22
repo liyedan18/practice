65a0cdbcbf309bf95b88952fecccb7bd7b098aa8
liyedan18 <544814490@qq.com>
2020-10-22 21:53:58 +0800

    剑指offer-p015   反转链表——递归解法

---
+++ offer/src/main/java/linkedlistcase/p015/Solution2.java
@@ -0,0 +1,39 @@
+package linkedlistcase.p015;
+
+/**
+ * 015   题目描述
+ * 输入一个链表，反转链表后，输出新链表的表头。
+ *
+ * https://www.nowcoder.com/practice/75e878df47f24fdc9dc3e400ec6058ca?tpId=13&&tqId=11168&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ *
+ *
+ *
+ * 递归解法
+ *
+ */
+public class Solution2 {
+
+    public ListNode ReverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        //返回反转链表后的头结点
+        ListNode res =  ReverseList(head.next);
+        //让头节点的下一个节点指向头节点
+        head.next.next = head;
+        //让头结点的next指向null,作为尾结点
+        head.next = null;
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        ListNode list = new ListNode(1);
+        list.next = new ListNode(2);
+        list.next.next = new ListNode(3);
+        System.out.println(solution.ReverseList(list).val);
+
+    }
+}
