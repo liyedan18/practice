490f67c6a49902d640637611153022c7bca8e6dc
liyedan18 <544814490@qq.com>
2021-02-11 14:43:27 +0800

    leetcode-p34. 在排序数组中查找元素的第一个和最后一个位置

---
+++ leetcode-my/src/main/java/com/leetcode/array/p000/p030/p034/Solution.java
@@ -0,0 +1,102 @@
+package com.leetcode.array.p000.p030.p034;
+
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 如果数组中不存在目标值 target，返回[-1, -1]。
+ *
+ * 进阶：
+ *
+ * 你可以设计并实现时间复杂度为O(log n)的算法解决此问题吗？
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 示例2：
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 示例 3：
+ *
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ *
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 105
+ * -109<= nums[i]<= 109
+ * nums是一个非递减数组
+ * -109<= target<= 109
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+ *
+ * 思路：
+ *      也就是寻找target的左边界和右边界
+ *      用二分搜索框架的边界搜索
+ *
+ */
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int left = leftBound(nums, target);
+        int right = rightBound(nums, target);
+
+        return new int[]{left, right};
+    }
+
+    private int leftBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                //收缩右边界
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        //返回的是left，根据上面判断left是否越界
+        if (left > nums.length - 1 || nums[left] != target) {
+            return -1;
+        }
+
+        return left;
+    }
+
+    private int rightBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                //收缩左边界
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        //返回的是right，根据上面判断right是否越界
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+
+        return right;
+    }
+
+}
