b4c924cb26f07bc4ff99af82c1a1686804bacdc9
liyedan18 <544814490@qq.com>
2021-02-12 15:23:52 +0800

    leetcode-p35. 搜索插入位置

---
+++ leetcode-my/src/main/java/com/leetcode/array/p000/p030/p035/Solution.java
@@ -0,0 +1,58 @@
+package com.leetcode.array.p000.p030.p035;
+
+/**
+ * 35. 搜索插入位置
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 示例2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-insert-position
+ *
+ * 思路：
+ *      直接用标准二分搜索
+ *      如果相等，则返回找到的mid
+ *      不相等，直接返回left，left的最终取值在[0,nums.length]闭区间满足要求
+ *
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+
+}
