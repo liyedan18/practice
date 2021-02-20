5530d33d304a5c152aace142d2e0e51a283d2e45
liyedan18 <544814490@qq.com>
2021-02-20 20:47:52 +0800

    剑指 Offer 05. 替换空格

---
+++ offer/src/main/java/stringcase/p05/Solution.java
@@ -0,0 +1,37 @@
+package stringcase.p05;
+
+/**
+ * 剑指 Offer 05. 替换空格
+ *
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ *
+ * 示例 1：
+ *
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *
+ *
+ * 限制：
+ *
+ * 0 <= s 的长度 <= 10000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+ *
+ * 思路：
+ *      将字符串转为字符，然后对每个字符做判断
+ */
+public class Solution {
+    public String replaceSpace(String s) {
+        // String res = s.replaceAll(" ", "%20");
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
