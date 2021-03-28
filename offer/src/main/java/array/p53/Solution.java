1f08da415eec3b152023632a42ac15c83a63681a
liyedan18 <544814490@qq.com>
2021-03-28 09:25:46 +0800

    剑指 Offer 53 - I. 在排序数组中查找数字 I

--- offer/src/main/java/array/p53/Solution.java
+++ offer/src/main/java/array/p53/Solution.java
@@ -1,62 +1,75 @@
 package array.p53;
 
 /**
- * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ * 剑指 Offer 53 - I. 在排序数组中查找数字 I
  *
- * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
- * 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ * 统计一个数字在排序数组中出现的次数。
  *
  * 示例 1:
  *
- * 输入: [0,1,3]
+ * 输入: nums = [5,7,7,8,8,10], target = 8
  * 输出: 2
- * 示例2:
+ * 示例 2:
  *
- * 输入: [0,1,2,3,4,5,6,7,9]
- * 输出: 8
- *
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: 0
+ *  
  *
  * 限制：
  *
- * 1 <= 数组长度 <= 10000
+ * 0 <= 数组长度 <= 50000
+ *
+ * 注意：本题与主站 34 题相同（仅返回值不同）：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
  *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof
+ * 链接：https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof
  *
  * 思路：
- *      已经排序，用二分法
- *      如果num[right]-nums[mid]==right-mid,则在左边，否则在右边
- *      最后left=mid时，判断结果
+ *      先用二分法查找到位置，然后向左边遍历和右边遍历
+ *      方法2,二分法求出左边界和右边界
  */
 public class Solution {
-    public int missingNumber(int[] nums) {
-        if (nums.length - 1 == nums[nums.length - 1]) {
-            return nums.length;
+    public int search(int[] nums, int target) {
+        //先用二分法查找到位置，
+        //然后向左边遍历和右边遍历
+        if (nums.length == 0 || nums[0] > target || nums[nums.length - 1] < target) {
+            return 0;
         }
 
+        int index = 0;
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (left == mid) {
-                if (nums[left] == left) {
-                    return left + 1;
-                } else {
-                    return left;
-                }
-            }
-            int temp = nums[right] - nums[mid];
-            int diff = right - mid;
-            if (temp == diff) {
-                //注意
-                right = mid;
+            if (nums[mid] == target) {
+                index = mid;
+                break;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                //注意
-                left = mid;
+                right = mid - 1;
             }
         }
+        if (nums[index] != target) {
+            return 0;
+        }
+        int count = 1;
+        int temp = index;
+        while (--temp >= 0 && nums[temp] == target) {
+            count++;
+        }
+        int rightTemp = index;
+        while (++rightTemp <= nums.length - 1 && nums[rightTemp] == target) {
+            count++;
+        }
+        return count;
 
-        return left;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.search(new int[]{5, 7, 7, 8, 8, 10}, 8));
+        System.out.println(s.search(new int[]{1, 4}, 4));
     }
 
 }
