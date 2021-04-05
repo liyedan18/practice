5fd74a8513321cfcd3af723035f45bd3e64d5105
liyedan18 <544814490@qq.com>
2021-04-05 12:29:04 +0800

    leetcode-p136. 只出现一次的数字

---
+++ leetcode-my/src/main/java/com/leetcode/bit/p136/Solution.java
@@ -0,0 +1,39 @@
+package com.leetcode.bit.p136;
+
+/**
+ * 136. 只出现一次的数字
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number
+ *
+ * 思路：
+ *      ^异或运算特点
+ *      A^A^B=B
+ *      A^0=A
+ *      A^A=0
+ *
+ */
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
